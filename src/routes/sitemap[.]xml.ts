126d4b090d8754bc4005f849e2b8472ddf650f62
David Darkdo <daviddarkdo@gmail.com>
2026-07-27 08:25:11 +0100

    BUILD J (SEO & Discovery): Add sitemap[.]xml.ts API route for root sitemap.xml

---
+++ src/routes/sitemap[.]xml.ts
@@ -0,0 +1,15 @@
+import { createAPIFileRoute } from "@tanstack/react-start/api";
+import { generateSitemapXml } from "@/lib/sitemapGenerator";
+
+export const APIRoute = createAPIFileRoute("/sitemap.xml")({
+  GET: async () => {
+    const xml = await generateSitemapXml();
+    return new Response(xml, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/xml; charset=utf-8",
+        "Cache-Control": "public, max-age=3600, s-maxage=86400",
+      },
+    });
+  },
+});
